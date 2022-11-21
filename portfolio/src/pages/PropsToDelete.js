e71b787ca8772871ae750630fe239027e513603a
ohsine <ohsine@korea.ac.kr>
2022-11-21 20:52:10 +0900

    1121_Update
    컴포넌트의 내용을 사용자가 수정할 수 있게 구현

--- portfolio/src/pages/PropsToDelete.js
+++ portfolio/src/pages/PropsToDelete.js
@@ -76,6 +76,39 @@ function Create(props)
     )
 }
 
+function Update(props)
+{
+    const [title, setTitle] = useState(props.title);
+    const [body, setBody] = useState(props.body);
+
+    return (
+        <article>
+        <h2>Update</h2>
+
+        <form onSubmit={(event) => {
+            event.preventDefault();
+            const title = event.target.title.value;
+            const body = event.target.body.value;
+            props.onUpdate(title, body);
+        }}>
+
+            <p><input type="text" name="title" value={title} onChange={(event) => {
+                setTitle(event.target.value);
+            }}/></p>
+
+            <p><textarea name="body" value={body} onChange={(event) => {
+                setBody(event.target.value);
+            }}></textarea></p>
+
+            <p><input type="submit" value="Update"></input></p>
+        
+        </form>
+
+    </article>
+    )
+}
+
+
 export default function PropsToDelete()
 {
     const [mode, setMode] = useState('WELCOME');
@@ -90,6 +123,7 @@ export default function PropsToDelete()
 
 
     let content = null;
+    let contextControl = null;
 
     if(mode === 'WELCOME')
     {
@@ -107,7 +141,13 @@ export default function PropsToDelete()
                 body = topics[i].body;
             }
         }
+
         content = <Article title={title} body={body}></Article>
+
+        contextControl = <li><a href={`/update` + id} onClick={(event) => {
+            event.preventDefault();
+            setMode('UPDATE');
+        }}>Update</a></li>
     }
     else if(mode === 'CREATE')
     {
@@ -118,11 +158,43 @@ export default function PropsToDelete()
 
         newTopics.push(newTopic);
         setTopics(newTopics);
-        
+
         setMode('READ');
         setId(nextId);
         setNextId(nextId + 1);
         }}></Create>
+    }
+    else if(mode === 'UPDATE')
+    {
+        let title, body = null;
+
+        for(let i = 0; i < topics.length; i++)
+        {
+            if(topics[i].id === id)
+            {
+                title = topics[i].title;
+                body = topics[i].body;
+            }
+        }
+
+        content = <Update title={title} body={body} onUpdate ={(_title, _body) => {
+
+            const newTopics = [...topics]
+            const updatedTopic = {id:id, title:_title, body:_body}
+
+            for(let i = 0; i < newTopics.length; i++)
+            {
+                if(newTopics[i].id === id)
+                {
+                    newTopics[i] = updatedTopic;
+                    break;
+                }
+            }
+            setTopics(newTopics);
+
+            setMode('READ');
+
+        }}></Update>
     }
 
     return (
@@ -140,11 +212,16 @@ export default function PropsToDelete()
 
             {content}
             <br />
-            <a href="/create" onClick={(event) => {
-                event.preventDefault();
-                setMode('CREATE');
-            }}>Create</a>
+            <ul>
+                <li>
+                    <a href="/create" onClick={(event) => {
+                    event.preventDefault();
+                    setMode('CREATE');
+                }}>Create</a>
+                </li>
 
+                {contextControl}
+            </ul>
         </div>
         
     )
